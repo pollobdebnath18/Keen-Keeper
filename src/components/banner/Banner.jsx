000fbf6ce7b1e35f0c24a90f197673c0b0262f08
pollob debnath <pullobdebnath@gmail.com>
2026-04-15 19:45:12 +0600

    banner with responsive

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+const Banner = () => {
+  return (
+    <div className="max-w-[1000px] mx-auto my-12 space-y-6">
+      <div className="text-center pt-5">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl text-[#1F2937] font-bold mx-4 md:mx-2 lg:mx-0">
+          Friends to keep close in your life
+        </h2>
+      </div>
+      <div className="text-center">
+        <p className="text-[16px] text-[#64748B] mx-4  md:w-[60%] md:mx-auto">
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <button className="btn text-white bg-[#244D3F] mx-auto">
+          <FaPlus></FaPlus> Add a Friend
+        </button>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-8 mx-4 md:mx-2">
+        <div className="flex flex-col justify-center items-center bg-white shadow-sm rounded-lg py-4 space-y-1.5">
+          <span className="text-xl font-bold text-[#244D3F]">10</span>
+          <span className="text-[#64748B] text-[14px]">Your Friends</span>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-white shadow-sm rounded-lg py-4 space-y-1.5">
+          <span className="text-xl font-bold text-[#244D3F]">10</span>
+          <span className="text-[#64748B] text-[14px]">Your Friends</span>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-white shadow-sm rounded-lg py-4 space-y-1.5">
+          <span className="text-xl font-bold text-[#244D3F]">10</span>
+          <span className="text-[#64748B] text-[14px]">Your Friends</span>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-white shadow-sm rounded-lg py-4 space-y-1.5">
+          <span className="text-xl font-bold text-[#244D3F]">10</span>
+          <span className="text-[#64748B] text-[14px]">Your Friends</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Banner from '../../components/banner/BannerPage';
+import React from "react";
+import Banner from "../../components/banner/Banner";
 
 const HomePage = () => {
-    return (
-        <div>
-            <Banner></Banner>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
 };
 
 export default HomePage;
